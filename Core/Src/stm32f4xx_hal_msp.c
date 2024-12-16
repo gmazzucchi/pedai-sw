20ff78cd4cd67dca8ad3d207e9c954ea802f641b
gmazzucchi <giacomo.mazzucchi@protonmail.com>
2024-12-16 22:48:43 +0100

    chore: update pins from PCB schematic

--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -98,7 +98,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc) {
     PA3     ------> ADC1_IN3
     PA4     ------> ADC1_IN4
     */
-        GPIO_InitStruct.Pin  = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4;
+        GPIO_InitStruct.Pin  = GPIO_PIN_1 | GPIO_PIN_2 | SOUND_ON_Pin | VOLUME_CONTROL_Pin;
         GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -129,7 +129,7 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc) {
     PA3     ------> ADC1_IN3
     PA4     ------> ADC1_IN4
     */
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4);
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1 | GPIO_PIN_2 | SOUND_ON_Pin | VOLUME_CONTROL_Pin);
 
         /* USER CODE BEGIN ADC1_MspDeInit 1 */
 
